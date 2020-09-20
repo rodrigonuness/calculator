5e06a17c65df61bf3a397513e6f4ce4a237904a4
Rodrigo Nunes <69399583+rodrigonuness@users.noreply.github.com>
2020-09-19 21:08:22 -0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,75 +1,75 @@
-var display = window.document.getElementById('display')
-var calculation = ""
-display.innerHTML = calculation
+var display = window.document.getElementById('display');
+var calculation = "";
+display.innerHTML = calculation;
 
 
 
 
 function fun1() {
-    calculation = calculation + "1"
-    display.innerHTML = calculation
+    calculation = calculation + "1";
+    display.innerHTML = calculation;
 }
 function fun2() {
-    calculation = calculation + "2"
-    display.innerHTML = calculation
+    calculation = calculation + "2";
+    display.innerHTML = calculation;
 }
 function fun3() {
-    calculation = calculation + "3"
-    display.innerHTML = calculation
+    calculation = calculation + "3";
+    display.innerHTML = calculation;
 }
 function fun4() {
-    calculation = calculation + "4"
-    display.innerHTML = calculation
+    calculation = calculation + "4";
+    display.innerHTML = calculation;
 }
 function fun5() {
-    calculation = calculation + "5"
-    display.innerHTML = calculation
+    calculation = calculation + "5";
+    display.innerHTML = calculation;
 }
 function fun6() {
-    calculation = calculation + "6"
-    display.innerHTML = calculation
+    calculation = calculation + "6";
+    display.innerHTML = calculation;
 }
 function fun7() {
-    calculation = calculation + "7"
-    display.innerHTML = calculation
+    calculation = calculation + "7";
+    display.innerHTML = calculation;
 }
 function fun8() {
-    calculation = calculation + "8"
-    display.innerHTML = calculation
+    calculation = calculation + "8";
+    display.innerHTML = calculation;
 }
 function fun9() {
-    calculation = calculation + "9"
-    display.innerHTML = calculation
+    calculation = calculation + "9";
+    display.innerHTML = calculation;
 }
 function fun0() {
-    calculation = calculation + "0"
-    display.innerHTML = calculation
+    calculation = calculation + "0";
+    display.innerHTML = calculation;
 }
 function funplus(){
-    calculation = calculation + "+"
-    display.innerHTML = calculation
+    calculation = calculation + "+";
+    display.innerHTML = calculation;
 }
 function funslash() {
-    calculation = calculation + "-"
-    display.innerHTML = calculation
+    calculation = calculation + "-";
+    display.innerHTML = calculation;
 }
 function fundiv() {
-    calculation = calculation + "/"
-    display.innerHTML = calculation
+    calculation = calculation + "/";
+    display.innerHTML = calculation;
 }
 function funrs() {
-    calculation = ""
-    display.innerHTML = calculation
+    calculation = "";
+    display.innerHTML = calculation;
 }
 function funx() {
-    calculation = calculation + "*"
-    display.innerHTML = calculation
+    calculation = calculation + "*";
+    display.innerHTML = calculation;
 }
 function funs() {
-    calculation = eval(calculation)
-    display.innerHTML = calculation
+    calculation = eval(calculation);
+    display.innerHTML = calculation;
 }
 function funp(){
-    calculation = calculation + "."
-    display.innerHTML = calculation
+    calculation = calculation + ".";
+    display.innerHTML = calculation;
 }
